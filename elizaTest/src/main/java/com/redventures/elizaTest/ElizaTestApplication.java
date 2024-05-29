137dd685443e893df2809a56f75668efe5da9a2a
elizapimentel <elizapimentel@hotmail.com>
2024-05-28 21:14:28 -0300

    first commit

---
+++ elizaTest/src/main/java/com/redventures/elizaTest/ElizaTestApplication.java
@@ -0,0 +1,13 @@
+package com.redventures.elizaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElizaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElizaTestApplication.class, args);
+	}
+
+}
