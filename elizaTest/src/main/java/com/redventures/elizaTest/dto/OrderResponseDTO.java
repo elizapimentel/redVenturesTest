397cccdb67d1b44729e7c31f27bb1ba3d252fec1
elizapimentel <elizapimentel@hotmail.com>
2024-06-01 20:26:38 -0300

    refact id to string

--- elizaTest/src/main/java/com/redventures/elizaTest/dto/OrderResponseDTO.java
+++ elizaTest/src/main/java/com/redventures/elizaTest/dto/OrderResponseDTO.java
@@ -5,14 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderResponseDTO implements Serializable {
 
-    private UUID id;
+    private String id;
     private String description;
     private String image;
 }
