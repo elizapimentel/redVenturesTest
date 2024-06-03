6bdd965c5cef4e36b3616331ca17d95170d8e7f5
elizapimentel <elizapimentel@hotmail.com>
2024-06-03 16:25:21 -0300

    commented

--- elizaTest/src/main/java/com/redventures/elizaTest/controllers/RamenItemsController.java
+++ elizaTest/src/main/java/com/redventures/elizaTest/controllers/RamenItemsController.java
@@ -1,39 +1,37 @@
-package com.redventures.elizaTest.controllers;
-
-import com.redventures.elizaTest.model.Broth;
-import com.redventures.elizaTest.model.Protein;
-import com.redventures.elizaTest.services.RamenItemsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("")
-public class RamenItemsController {
-
-    @Qualifier("ramenItemsServiceImpl")
-    @Autowired
-    private RamenItemsService service;
-
-    @GetMapping("/broths")
-    public ResponseEntity<List<Broth>> getAllBroths(
-            @RequestHeader(value = "x-api-key", required = true) String authorizationHeader) {
-        try {
-            List<Broth> products = service.getAllBroths();
-            return ResponseEntity.status(200).body(products);
-        } catch (Error e) {
-            return ResponseEntity.status(403).build();
-        }
-    }
-
-    @GetMapping("/proteins")
-    public ResponseEntity<List<Protein>> getAllProteins(
-            @RequestHeader(value = "x-api-key", required = true) String authorizationHeader) {
-        List<Protein> products = service.getAllProteins();
-        return ResponseEntity.status(200).body(products);
-    }
-
-}
+//package com.redventures.elizaTest.controllers;
+//
+//import com.redventures.elizaTest.model.Broth;
+//import com.redventures.elizaTest.model.Protein;
+//import com.redventures.elizaTest.services.RamenItemsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("")
+//public class RamenItemsController {
+//
+//    @Qualifier("ramenItemsServiceImpl")
+//    @Autowired
+//    private RamenItemsService service;
+//
+//    @PostMapping("/postBroth")
+//    public ResponseEntity<Broth> postBroth(@RequestBody Broth broth) {
+//        try {
+//             return ResponseEntity.status(201).body(service.postNewBroth(broth));
+//        } catch (Error e) {
+//            return ResponseEntity.status(402).build();
+//        }
+//    }
+//
+//    @PostMapping("/postProtein")
+//    public ResponseEntity<Protein> postBroth(@RequestBody Protein protein) {
+//        try {
+//            return ResponseEntity.status(201).body(service.postNewProtein(protein));
+//        } catch (Error e) {
+//            return ResponseEntity.status(402).build();
+//        }
+//    }
+//
+//}
