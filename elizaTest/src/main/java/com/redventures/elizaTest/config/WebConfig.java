5825a2e50affbb506b492a9f6eec07af74121845
elizapimentel <elizapimentel@hotmail.com>
2024-06-03 18:15:19 -0300

    uncommitted

---
+++ elizaTest/src/main/java/com/redventures/elizaTest/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.redventures.elizaTest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://tech.redventures.com.br", "http://localhost:8090", "https://api.tech.redventures.com.br", "https://5d2d-179-251-190-191.ngrok-free.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
